2c28c7b523bb2782cf442159d8074484999d74ab
Riju <mailmeonriju@gmail.com>
2020-09-02 03:37:33 +0530

    In keka_login parameterised start_date, end_date and cookie, skipped weekends

--- work/keka_login.py
+++ work/keka_login.py
@@ -1,11 +1,17 @@
 import json
+import click
 import requests
 from loguru import logger
 from datetime import datetime, timedelta
 
-def main():
+
+@click.command()
+@click.option('--cookie', required=True, help='login cookie value is required for authentication')
+@click.option('--start_date', required=True, help='date starting which attendance needs to be marked')
+@click.option('--end_date', required=True, help='date till which attendance needs to be marked')
+def main(cookie, start_date, end_date):
     url = "https://decisiontree.keka.com/api/mytime/attendance/saveattendanceadjustment"
-    
+
     headers = {
         'authority': 'decisiontree.keka.com',
         'accept': 'application/json, text/plain, */*',
@@ -19,24 +25,68 @@ def main():
         'sec-fetch-dest': 'empty',
         'referer': 'https://decisiontree.keka.com/ui/',
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'cookie': 'Subdomain=decisiontree.keka.com; ThemeIndex=7; ASP.NET_SessionId=o2ml14pl42pb2rbevjadai15; .AspNet.Cookies=h5rZZ0JJFHmEu3TFo9apXshAat17jrY-r6Legs7vGtPRzLJtw6XT1AnycW4oHlRalckLfG1M2rpExjrOCt99kuipTkc67uD92p-1ug4QR3BM_m6MWZdF6uY5ePeM7SKGczGsP4TAvSHMQ2rk9Nikmox3RFSOw_YLX5t6FHw5xR1e7BbR8lG5FV46yv08IAN_q3ptBOYtkZ9YFzLOkAUnfRLsIOKjm08MpRaq-ztj5JcEARvRmhKrRr9l0hHv2MTpOK2pMpdmD4erHUUwxW_PSUcjwhVLCfZEWX1hJwG0rIk1C58iGz1cnuF9x2k0UuEY3jg_MHpEuLCANAHAt60S46dkGEJ5QzBsasAQ1FcnEGgUFxF37YI--_w8YviQER3Yt-8cB50XIoq92ttZv2jOrr5DENEeqzrBFJp_JjOElc3w_mUrpYYfnlJEjCrcc6kBOStN1sruWN5_ksRDYHnylPKaWFw-GxGRX-xuv_zCRhfxLZUVlzkMMk7Mc8RCagbjpV6X08RnAz6Fp4MgbjFG3XrzQXsbOimBglHw3j9CnAQ98FefBGsVppklNNw7CYd5CGCJ9g3hRlceBqEF0ugah5T3eCUa9zgXQTD9mjsCu9_14Ou1QeVSYaL_wo5USpmVHbU3mNDr0HBnEI1Y8cl6uYuZdsKx_93dJJsOD6R7p8Rg1pJhlknOppwL_EsvxWElJWrsLbyDB5r603l13313U_f_XbW_ouWomdHjcKbm7BN9w4sf_URMBlBd3Br8ArrAyf52f46k4Win7MHSsFNIAwXDUH9CB_FVqvWOvMervOoM62w5r6Dm-8bbYlg3C-IRX6VzCTpuYOske4W9cJnmiHNV1HIKkYhRNnLEja2_7eWcczVkJsiftyBzyGwZDCP08_tUiFYj6aGli-NB-ZI6i30FoCUFHGz9lN9iZzbQ4F2ZySAM5SltP0-SOb6AG0KxCbaWoXmM-SSHD3JyZSAG3TRxjn5tO83lfgdiiGYFpC_FUnfT5u8j56uSI5Pdwy0_ibH8IMZ5CebCo3RiNSfKu8SgPufGzo-LEnsdRxQcVMsF8iMZVZfO2CO5rmFSZVbx4rR7DCtYPlibOthWjAkMygAtB86x9Qu5Gp2nbu0ZehfvpXk0LHIE42jwtT5pkgzcNf2fHrwy6atHMjT0-qGsVfpriuTTHiY1soNAUU_QDUW8yyce1EKwXuYb3Oo456Y5p6V63i5P-mZZnv8PaBtMEOshTjfQuFWAbc0VpwbBky-7V7QQCexArHzU3VyHo3Fx15OrnLgQUGYj4H3KZoojmbrPwGLBY-PPFKK3LnlBKjGmI1Sr70XX4ur5uyTp_fTNfckpt3pmg1UU6z-H2NDARd45yWh75_kl87oBmSrU9l37QDtBAm0zVShRXodYbu5Azmk-25RydRC-ruV3YHj704LQnfDZ8ZgCSNNInNMrYrclBQwjudVdWQGSYJ4gRw8I9OqLvcCFLjKms47zI950zdIzZNlHedw6D7tugIwD7MhhyJCg_Cc_zvdEX52wUtrkij5Tbu1oED37ajMyMYuDo4tX_30e97QqoxmLkmlzWRP8idRP6gr272CZHbH9iIl_O4ZpnvzvAikUx4_Zg3T4kiutfLQZLstNFNsZUFd7RXLhp1S_bQmwyq_EQ22Brpsrl5-KKmZNp9NsFxuDBLnHThsGxG-YpQfnX6ME_LaP0qHvJlsbnBQ3n5hu7wI3iQUCSmHSP6pqyuLNpmrz4H0Bw0BUMD-O8a8x0W5Qlqlx3Hd6P0KgSCkRdc8nUKSLaqobWudj2w3L88FWT0OzqLG92XIuJBbmJ3n-GsD6IPzuCPx2RMhzfHlAr-lVi6exAvd5yeSXzr2e-IWH6UyQa4NM9GfhRomsbFHUn0pBWZ-zjpyV8UBH6GtJmV6fmOaVuWReFtY6Ma7GQ6yF0TbsIg1drmGSGe0rGw7ann9X_wgnjCndWLl_uHpARdeCcGzOyXGMTNMlstXXjJIgQh6jCp4ZTUwl8pC8aKDZ6ju6_LZwqe2eoV09xoJrOPxwg-zZrG690faK6GpMLirMWlDO_CNqI7UuQCkpAjap8nGto3N4brn7a4KzS3H9HBW_1rl_MZw2A0vSnF8Fq2A91-2d6OOaalxMABNDclNoZGwv9MTRkO_JkilHZdwh6GYI8_9j28qb'
+        'cookie': cookie
     }
-    start_date = datetime.strptime('20200622', '%Y%m%d')
-    end_date = datetime.strptime('20200624', '%Y%m%d')
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
     n_dates_diff = (end_date - start_date).days + 1
     current_date = start_date
-    for i in range(n_dates_diff):
-        
+    for _ in range(n_dates_diff):
+        if (current_date.isoweekday() in (6, 7)):
+            logger.warning(
+                'Skipping due to weekend on {}'.format(current_date))
+            current_date += timedelta(days=1)
+            continue
         payload = json.dumps({
-            "timeEntries": [],
+            "timeEntries": [
+                {
+                    "actualTimestamp": "{}T10:00:00.000Z".format(
+                        datetime.strftime(current_date, '%Y-%m-%d')
+                    ),
+                    "adjustedTimestamp": "{}T10:00:00.000Z".format(
+                        datetime.strftime(current_date, '%Y-%m-%d')
+                    ),
+                    "timestamp": "{}T10:00:00.000Z".format(
+                        datetime.strftime(current_date, '%Y-%m-%d')
+                    ),
+                    "originalPunchStatus": 0,
+                    "modifiedPunchStatus": 0,
+                    "punchStatus": 0,
+                    "attendanceLogSource": 1,
+                    "premiseName": "Attendance Adjustment",
+                    "manualClockinType": 2,
+                    "isAdjusted": True,
+                    "isEditable": True,
+                    "isManuallyAdded": True
+                },
+                {
+                    "actualTimestamp": "{}T18:00:00.000Z".format(
+                        datetime.strftime(current_date, '%Y-%m-%d')
+                    ),
+                    "adjustedTimestamp": "{}T18:00:00.000Z".format(
+                        datetime.strftime(current_date, '%Y-%m-%d')
+                    ),
+                    "timestamp": "{}T18:00:00.000Z".format(
+                        datetime.strftime(current_date, '%Y-%m-%d')
+                    ),
+                    "originalPunchStatus": 1,
+                    "modifiedPunchStatus": 1,
+                    "punchStatus": 1,
+                    "attendanceLogSource": 1,
+                    "premiseName": "Attendance Adjustment",
+                    "manualClockinType": 2,
+                    "isAdjusted": True,
+                    "isEditable": True,
+                    "isManuallyAdded": True
+                }
+            ],
             "note": ".",
             "employeeId": 33992,
             "requestDate": datetime.strftime(current_date, '%Y-%-m-%-d'),
             "requestType": 7
         })
         current_date += timedelta(days=1)
-        
-    
+
         logger.info('requesting for {}'.format(payload))
 
         response = requests.request("POST", url, headers=headers, data=payload)
@@ -46,3 +96,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+'''
+Usage:
+python keka_login.py --cookie='Subdomain=decisiontree.keka.com; ThemeIndex=7; ASP.NET_SessionId=mwz4gvjphgh2cnly54torhvm; .AspNet.Cookies=leHYw9xzyygkARY-tCnSPsIIJjDZxc-Tidq1MW_WCMgWAvlTmUJbTOtJnvVR7Zo7vzpLrdYM9Yrq_KwRQGL5PmUqBeNZq3q_pym9hvWpohKsrVTfjjwSRfpNHq9feFXZfOqrBg29hfTtEAoZ2q5443c5C315motIruHboKOqNNt53d8G9cyg89jRXgESo0liRYE7CWhlHPYXhw8hcO-3OTAjAYXsG6ZTRsnBvyPto_CfpXSGR9HyLWZM8iAI6cQThE4R9rgwEdMT-Oz9u1S74xbVdlMGfP_5qUkj6ZJfSMdjIu3MLuLhCctVY24kdRi7xbbcpfqayq6oiTQeLcFWxf_2K72bQSytzBhi18Gl5D0w7fsgfPukE0KylZTRczEuZ8Eegsx1RgKiYgbZNMaCGwBi8hoa5CBk5uYIfQ0morUOapnNbO0Zz2umC5M9e0zE7gFQZLfvDPxMLAosCGMyVEQHVgVK15ctU4j8XHVgqbZY_uFddIqNYfDY2klxz5Uu7MNEXOux9iLCKNoSIQkMV3qbDWHv6NQEM4PW1S44FSm6S0H8vtOoD7VCD9WRx_SXcDKSGDaTFQilmof9PO0jJgjYXv7_-8ZykTZphSiT8lwZ2P49F5BzRrHstvLYuXlvXRcDaSfRxBtk_HsBifGr8ePw4UHhA37VJY4JlnIt5P3-QGG6-F06Tx7rLH3xsJeHx6Ly4G20UCzeRjlduMsoAuVi-a3i6Lp1Rotv2g3nzq1w4OoshEW0fXdMXvprx3LPA-L-tFxyiTEmRx5Td3oQfOZxraYEW4HdypstYjpPH8s8guByulHqKzNmXUo5Ykr_-eA-hZmkJJuNE3k4JSC3mzcQJtylv5U7udwp-n0_ESvlWhLwXLK7HTXLbvROgaCpyjVMAeLxzIbya3q0sbxoavGuyQYOoWeMQvVYUmOwMHQnwnitz5Lf60MYZkaIDCEysVGSmkyJ4UfqB1eAbt3sN1qBR9opU0SMrRBgN9moObQ4YVpjq9ijTLxuUIwd8bXfm2rp992IWM00FS_awQjla8NkIv-pdjTsdY_ODyCOlVYahCRSiGictLznguS3npeSxtljDKZsbk93A6l_a7_VIVCjgdp5KYaXPOofJsWJ3RaQvjFYPL87wjTmPgG2TrycI0x2Q_zbkZF30ipw2ONj14IzjkaUK0IMPqS9tsY0danzh_lsn23OUB3YHLEVrhJWlzXbIg5yGT8Je3Vcu7vU6seRbKuGHOUJOYzH-tTiBgjw-tZD5PEU77qfkyGmT8pWcgQceDf9RyY3OwHVWUTqdfF0V1PGuZcIYSS0oAzw7T3pCsgGcMMgaj6QS1cIbHE6Pe8buWR-36Qrqe30Uev7F3Y3qP5imJXba2tkkOnVGsFHoaxhcOBMnqLQPGSX1vVS2dZCoX7H6642pMG0z-I-1AyZrYVx6eVwjoSPIqO4VxyE24Gb5DsC_aitbPtIUHKY2tybkbH7yR8JkTj6yUO7OaC90oPkcvfOKmnEliwOkWBHHhlGtVn_q0MTEfEne-XyCj5StpPw6rTJ83z8uOA12W9qclQN_8JZzteq67ZjuiCgRDnObR5jiA1gM37cQZZ8ZfI2BdeaDXENGGQALbjclVlecQIHyFLaNlmhIy-fJorsmDWWsjjq8hv6fxR-yFCGycRGxfPTYFTez14o_smutYbVbteKVtzE7HbJrKgxd4PgNtPJdJL8IVWCSiT5IBI56LoPevkCHo7lwFv65ZpY07Zecnw9Z4LhuVrPXl9OpLmtqos_htCKZI_NkZW2NDRFrQ9_xR-UyAvyizwUD80sqDPT6Pwq_n3HJckGXZrafIKajvFmQ03O3Nq7Q6tcx1CKd9onFwBJDu96g1vpEq528SIPGfhhsYPM91St3AYEbt0wRiJ25d73G_m5uuI3nmgX2zwRCRAYGeAM0uQpRyITHKDViGkig5-xB0_hTI_C32b7zpmrAQRtq-0HBf0R-RzmDYEZ7JK0TSqdl0ybr8gtqGUppdUdMykefSUpYnphZCiajOM79kfhv1_Ko74yZwGYM2F0SrSuKewJ1gSOw7uEVXXU-1a27OQkRmNVuJeHezXsKBiGCXxCm6jnrgfNyUYaCGlPpgRvdY2RbjBZZN-BwCUJfMqrr34IhI_T7NY8Fmw' --start_date=2020-08-24 --end_date=2020-08-31
+'''
