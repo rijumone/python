79d94ad0703c3cdcad89b7d9a6be31155a1ac26b
Riju's Desktop Workstation <riju@desktop.wkstn>
2021-04-11 11:48:13 +0530

    added docstrings to tests

--- interview/finn.auto/url-shortening-service/mk1/tests/functional/test_decode.py
+++ interview/finn.auto/url-shortening-service/mk1/tests/functional/test_decode.py
@@ -2,16 +2,28 @@ from conftest import client
 
 
 def make_encode_request(client):
+    """
+    Helper function to make the /encode request
+    """
     return client.post(
         '/encode', json={'url': 'https://codesubmit.io/library/react'})
 
 
 def make_decode_request(client, short_url):
+    """
+    Helper function to make the /decode request
+    """
     return client.post(
         '/decode', json={'short_url': short_url})
 
 
 def test_json_return(client):
+    """
+    GIVEN the /decode endpoint
+    WHEN a valid request is made
+    THEN check the response is JSON
+    """
+
     encode_res = make_encode_request(client)
     encode_json = encode_res.get_json()
     short_url = encode_json['data']
@@ -26,6 +38,13 @@ def test_json_return(client):
 
 
 def test_response_structure(client):
+    """
+    GIVEN the /decode endpoint
+    WHEN a valid request is made
+    THEN check the response contains `message`
+        and `data` keys
+    """
+
     encode_res = make_encode_request(client)
     encode_json = encode_res.get_json()
     short_url = encode_json['data']
@@ -39,6 +58,12 @@ def test_response_structure(client):
 
 
 def test_response_success(client):
+    """
+    GIVEN the /decode endpoint
+    WHEN a valid request is made
+    THEN check the value of `message` is success
+    """
+
     encode_res = make_encode_request(client)
     encode_json = encode_res.get_json()
     short_url = encode_json['data']
@@ -51,6 +76,13 @@ def test_response_success(client):
 
 
 def test_decode_functionality(client):
+    """
+    GIVEN the /decode endpoint
+    WHEN a valid request is made
+    THEN check if the original URL returned in response
+        matches the URL sent to /encode earlier
+    """
+
     encode_res = make_encode_request(client)
     encode_json = encode_res.get_json()
     short_url = encode_json['data']
@@ -62,18 +94,39 @@ def test_decode_functionality(client):
 
 
 def test_invalid_request(client):
+    """
+    GIVEN the /decode endpoint
+    WHEN an invalid request is made
+    THEN the response contains the `error` key and
+        check the response code is 400
+    """
+
     response = client.post('/decode', json={})
     json_data = response.get_json()
     assert 'errors' in json_data
+    assert response.status_code == 400
 
 
 def test_non_existent_url(client):
-    # add functionality to add 404 code
+    """
+    GIVEN the /decode endpoint
+    WHEN a non-existent short URL is passed
+    THEN the response contains the `error` key and
+        check the response code is 404
+    """
+
     response = client.post('/decode', json={'short_url': 'foobarqux'})
     json_data = response.get_json()
     assert 'errors' in json_data
+    assert response.status_code == 404
 
 
 def test_other_request_verbs_failing(client):
+    """
+    GIVEN the /decode endpoint
+    WHEN invalid methods are requested
+    THEN check the response code is 405
+    """
+
     response = client.get('/decode', json={})
     assert response.status_code == 405
--- interview/finn.auto/url-shortening-service/mk1/tests/functional/test_encode.py
+++ interview/finn.auto/url-shortening-service/mk1/tests/functional/test_encode.py
@@ -2,20 +2,35 @@ from conftest import client
 
 
 def make_request(client):
+    """
+    Helper function to make the /encode request
+    """
     return client.post(
         '/encode', json={'url': 'https://codesubmit.io/library/react'})
 
 
 def test_json_return(client):
+    """
+    GIVEN the /encode endpoint
+    WHEN a valid request is made
+    THEN check the response is JSON
+    """
+
     response = make_request(client)
     try:
         response.get_json()
     except TypeError:
         assert False
-    assert True
+    assert response.status_code == 200
 
 
 def test_len_shorturl(client):
+    """
+    GIVEN the /encode endpoint
+    WHEN a valid request is made
+    THEN check the shortened url contains the hash
+        exactly 5 characters long
+    """
 
     response = make_request(client)
 
@@ -25,6 +40,13 @@ def test_len_shorturl(client):
 
 
 def test_response_structure(client):
+    """
+    GIVEN the /encode endpoint
+    WHEN a valid request is made
+    THEN check the response contains `message`
+        and `data` keys
+    """
+
     response = make_request(client)
 
     json_data = response.get_json()
@@ -34,6 +56,12 @@ def test_response_structure(client):
 
 
 def test_response_success(client):
+    """
+    GIVEN the /encode endpoint
+    WHEN a valid request is made
+    THEN check the value of `message` is success
+    """
+
     response = make_request(client)
 
     json_data = response.get_json()
@@ -41,16 +69,27 @@ def test_response_success(client):
     assert json_data['message'] == 'success'
 
 
-def test_other_request_verbs_failing(client):
+def test_other_request_method_failing(client):
+    """
+    GIVEN the /encode endpoint
+    WHEN invalid methods are requested
+    THEN check the response code is 405
+    """
+
     response = client.get('/encode', json={})
     assert response.status_code == 405
 
 
 def test_invalid_request(client):
+    """
+    GIVEN the /encode endpoint
+    WHEN an invalid request is made
+    THEN the response contains the `error` key and
+        check the response code is 400
+    """
+
     response = client.post(
         '/encode', json={'url1': 'https://codesubmit.io/library/react'})
     json_data = response.get_json()
     assert 'errors' in json_data
     assert response.status_code == 400
-
-# test_json_return(client)
